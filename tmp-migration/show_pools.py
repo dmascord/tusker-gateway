a8f52e97ee9e5f9ffadd0947bba99422699e31bd
Damien Mascord <damien.mascord@sita.aero>
2026-08-21 10:57:47 +1000

    chore: remove tmp-migration files

--- tmp-migration/show_pools.py
+++
@@ -1,7 +0,0 @@
-from tusker_gateway.config import load_config
-c = load_config()
-print("=== CURRENT POOLS ===")
-for n, p in c.get("pools", {}).items():
-    print("--- pool:", n, "---")
-    for m in p.models:
-        print("  -", m.get("provider"), "/", m.get("model"))
